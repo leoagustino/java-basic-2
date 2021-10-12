359b51f7eb5e30dc5cdaefe093ef9f201ac8570a
leoagustino <gunyearsofficial@gmail.com>
2021-10-12 16:21:53 +0700

    leo

---
+++ biodataleo.java
@@ -0,0 +1,13 @@
+public class biodataleo {
+    public static void main(String[] args){
+        String firstname ="Leo", lastname ="Agustino", alamat = "jl.pintukemana saja no 1", phone = "081288168891", hobi="Main game";
+        int Age = 21;
+        
+
+        System.out.println("==========================================================");
+        System.out.println("||Firstname : "+firstname+"\n||Lastname : "+lastname+"\n||Phone Number :"+
+        phone+"\n||Address : "+alamat
+        +"\n||Hobi : "+hobi+"\n||Age : "+Age);
+        System.out.println("==========================================================");
+    }
+}
